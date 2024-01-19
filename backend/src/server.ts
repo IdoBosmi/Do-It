7c8d39bcb1584b8894cc8fca62eadfa1bfed1deb
Ido Bosmi <ido.bosmi@gmail.com>
2024-01-19 09:17:19 +0200

    backend8

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,15 +6,20 @@ import app from "./app";
 
 const port = 5000; //process.env.PORT;
 
-
-mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
-.then(()=>{
-    console.log("Mongoose connected...");
-    app.listen(port, ()=>{
-        console.log("Server running on port: " + port);
-    })
+app.listen(port, ()=>{
+    console.log("Server running on port: " + port);
 })
-.catch(console.error)
+
+
+
+// mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+// .then(()=>{
+//     console.log("Mongoose connected...");
+//     app.listen(port, ()=>{
+//         console.log("Server running on port: " + port);
+//     })
+// })
+// .catch(console.error)
 
 
 
