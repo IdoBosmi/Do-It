c7e297df3f6b6b52b202039eec4fc7b383f67e7b
Ido Bosmi <ido.bosmi@gmail.com>
2024-01-21 21:39:25 +0200

    test1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,81 +1,77 @@
-// import { useEffect, useState } from 'react';
-// import { UserModel } from './models/user';
-// import * as TaskAPI from "./network/tasks_api";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import NotFoundPage from './pages/NotFoundPage';
-// import MyTasksPage from './pages/MyTasksPage';
-// import Header from './components/Header';
-// import LoginModal from './components/LoginModal';
-// import SignUpModal from './components/SignupModal';
+import { useEffect, useState } from 'react';
+import { UserModel } from './models/user';
+import * as TaskAPI from "./network/tasks_api";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFoundPage from './pages/NotFoundPage';
+import MyTasksPage from './pages/MyTasksPage';
+import Header from './components/Header';
+import LoginModal from './components/LoginModal';
+import SignUpModal from './components/SignupModal';
 
 function App() {
 
 
-  // const [showSignUpModal, setShowSignUpModal] = useState(false);
-  // const [showLoginModal, setShowLoginModal] = useState(false);
-  // const [loggedInUser, setLoggedInUser] = useState<UserModel | null>(null);
+  const [showSignUpModal, setShowSignUpModal] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState<UserModel | null>(null);
 
 
-  // useEffect(() => {
-  //   async function fetchLoggedInUser() {
-  //     try {
-  //       const user = await TaskAPI.getLoggedInUser();
-  //       setLoggedInUser(user);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-  //   fetchLoggedInUser();
-  // }, []);
+  useEffect(() => {
+    async function fetchLoggedInUser() {
+      try {
+        const user = await TaskAPI.getLoggedInUser();
+        setLoggedInUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchLoggedInUser();
+  }, []);
 
 
   return (
+    <BrowserRouter>
+      <div>
+        <Header
+          loggedInUser={loggedInUser}
+          onLoginClick={() => setShowLoginModal(true)}
+          onSignupClick={() => setShowSignUpModal(true)}
+          onLogoutSuccessful={() => setLoggedInUser(null)}
+        />
 
-    <div>
-      Hello Ido
-    </div>
-    // <BrowserRouter>
-    //   <div>
-    //     <Header
-    //       loggedInUser={loggedInUser}
-    //       onLoginClick={() => setShowLoginModal(true)}
-    //       onSignupClick={() => setShowSignUpModal(true)}
-    //       onLogoutSuccessful={() => setLoggedInUser(null)}
-    //     />
+        <Routes>
+          <Route
+            path="/"
+            element={<MyTasksPage loggedInUser={loggedInUser} />}
+          />
 
-    //     <Routes>
-    //       <Route
-    //         path="/"
-    //         element={<MyTasksPage loggedInUser={loggedInUser} />}
-    //       />
+          <Route
+            path="/*"
+            element={<NotFoundPage />}
+          />
+        </Routes>
 
-    //       <Route
-    //         path="/*"
-    //         element={<NotFoundPage />}
-    //       />
-    //     </Routes>
+        {showSignUpModal &&
+          <SignUpModal
+            onDismiss={() => setShowSignUpModal(false)}
+            onSignUpSuccessful={(user) => {
+              setLoggedInUser(user);
+              setShowSignUpModal(false);
+            }}
+          />
+        }
+        {showLoginModal &&
+          <LoginModal
+            onDismiss={() => setShowLoginModal(false)}
+            onLoginSuccessful={(user) => {
+              setLoggedInUser(user);
+              setShowLoginModal(false);
+            }}
+          />
+        }
 
-    //     {showSignUpModal &&
-    //       <SignUpModal
-    //         onDismiss={() => setShowSignUpModal(false)}
-    //         onSignUpSuccessful={(user) => {
-    //           setLoggedInUser(user);
-    //           setShowSignUpModal(false);
-    //         }}
-    //       />
-    //     }
-    //     {showLoginModal &&
-    //       <LoginModal
-    //         onDismiss={() => setShowLoginModal(false)}
-    //         onLoginSuccessful={(user) => {
-    //           setLoggedInUser(user);
-    //           setShowLoginModal(false);
-    //         }}
-    //       />
-    //     }
-
-    //   </div>
-    // </BrowserRouter>
+      </div>
+    </BrowserRouter>
   );
 };
 
