52b357283d68495821b0846ec21c7ca58fb8829e
Ido Bosmi <ido.bosmi@gmail.com>
2024-02-18 18:28:17 +0200

    updated task interface

---
+++ backend/src/interfaces/task.ts
@@ -0,0 +1,12 @@
+import { Types } from "mongoose";
+
+export interface TaskInterface {
+    _id: Types.ObjectId,
+    userId: Types.ObjectId, //???
+    taskListId?: string | null, 
+    title: string,
+    createdAt: NativeDate,
+    updatedAt: NativeDate,
+    dueDate: Date,
+    isCompleted: boolean
+}
